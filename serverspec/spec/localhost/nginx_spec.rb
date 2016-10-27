e24d37344f7019b362227ed716b1f52c84d7c33f
oomatomo <ooma0301@gmail.com>
2016-10-28 00:52:58 +0900

    serverspecを分割

--- serverspec/spec/localhost/nginx_spec.rb
+++ serverspec/spec/localhost/nginx_spec.rb
@@ -1,14 +1,5 @@
 require 'spec_helper'
 
-describe service('jenkins') do
-  it { should be_enabled }
-  it { should be_running }
-end
-
-describe port(8000) do
-  it { should be_listening }
-end
-
 describe service('nginx') do
   it { should be_enabled }
   it { should be_running }
